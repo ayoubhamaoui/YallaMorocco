d6aa3807b88e5a714cd14693c981528a601e374a
ayoubhamaoui <ayoub10hamaoui@gmaiL.com>
2017-12-27 03:09:16 +0000

    first commit

---
+++ assets/js/angular/module.js
@@ -0,0 +1,65 @@
+var module=angular.module('myapp',['ngRoute']);
+
+module.config(function($routeProvider){
+  $routeProvider.when('/home',{
+    templateUrl:'view/home.html',
+  })
+  .when('/list',{
+    templateUrl:'view/page2.html',
+    controller:'myctr',
+  })
+  .when('/start',{
+    templateUrl:'app/start.html',
+    controller:'myctr',
+  })
+  .otherwise({redirectTo:'/index.html'});
+
+});
+module.controller("myctr",function($scope){
+   $scope.person=[
+       {
+           name:'ayoub',
+           age:20,
+           color:"red",
+       },
+       {
+           name:'amine',
+           age: 30,
+           color: "black",
+       },
+       {
+           name:'Ali',
+           age: 17,
+           color: "blue",
+       },
+       {
+           name:'Selma',
+           age: 19,
+           color: "yellow",
+       },
+       {
+           name:'Imane',
+           age: 21,
+           color: "blue",
+       }
+     ];
+    $scope.del=function(x)
+    {
+      var i=$scope.person.indexOf(x);
+      $scope.person.splice(i,1);
+    }
+    $scope.addnew=function()
+    {
+      $scope.person.push(
+        {
+          name:$scope.new.name,
+          age:$scope.new.age,
+          color:$scope.new.color,
+        }
+      );
+      $scope.new.name='';
+      $scope.new.age='';
+      $scope.new.color='';
+    }
+
+});
